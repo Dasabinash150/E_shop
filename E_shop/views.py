a3bf521fec9d5fbbc7f86f2c6c0095bd47b08a97
Dasabinash150 <dasabinash150@gmail.com>
2024-04-02 16:18:32 +0530

    signup form craete

--- E_shop/views.py
+++ E_shop/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 
 # from django.template import Template , Context
 from app.models import Category,Product
+from django.contrib.auth import authenticate,login
+from app.models import UserCreateForm
 
 
 def Master(request):
@@ -25,3 +27,23 @@ def homePage(request):
         'product': product,
     }
     return render(request, "index.html", context )
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            new_user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+            )
+            login(request, new_user)
+            return redirect('index')
+    else:
+        form = UserCreateForm()
+
+        
+    context = {
+            'form':form,
+        }
+    return render(request,'registration/signup.html',context)
--- app/models.py
+++ app/models.py
@@ -35,3 +35,27 @@ class Product(models.Model):
         return self.name
 
 class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True,label='Email',error_messages={'exists':'This Already Exists'})
+
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] ='Password'
+        self.fields['password2'].widget.attrs['placeholder'] ='Confirm Password'
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user 
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data['email']).exists():
+            raise forms.ValidationError(self.fields['email'].error_message['exists'])
+        return self.cleaned_data['email']
