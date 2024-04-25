d7924059a562b29686caacc39a4f472222fe4343
Dasabinash150 <dasabinash150@gmail.com>
2024-04-25 09:59:12 +0530

    cart created

--- E_shop/settings.py
+++ E_shop/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 'app.apps.AppConfig',
-    'app'
+    'app',
+
+    'cart',
 ]
+
+CART_SESSION_ID = 'cart'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
--- E_shop/views.py
+++ E_shop/views.py
@@ -7,6 +7,11 @@ from app.models import Category,Product,Contact_us
 from django.contrib.auth import authenticate,login
 from app.models import UserCreateForm
 
+# add to cart
+
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
+
 
 def Master(request):
     return render(request, "master.html")
@@ -59,3 +64,47 @@ def contact(request):
 
         contact.save()
     return render(request, 'contact.html')
+
+
+@login_required(login_url="/accounts/login/")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required(login_url="/accounts/login/")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/accounts/login/")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
